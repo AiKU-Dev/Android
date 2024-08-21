e414931d5cd6123f4cc26072add926e6b356cdb7
Strone <sangbbang31@gmail.com>
2024-08-21 23:40:37 +0900

    UiState 예시

--- core/src/main/java/com/aiku/core/base/BaseViewModel.kt
+++ core/src/main/java/com/aiku/core/base/BaseViewModel.kt
@@ -1,11 +1,16 @@
 package com.aiku.core.base
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
 
@@ -25,5 +30,44 @@ abstract class BaseViewModel: ViewModel() {
         _uiState.value = UiState.Loading
 
         block()
+    }
+}
+
+@HiltViewModel
+class LoginViewModel @Inject constructor(
+    userDelegate: UserDelegate
+) : BaseViewModel(), UserDelegate by userDelegate {
+
+    private val _login: MutableSharedFlow<LoginResultModel> = MutableSharedFlow()
+    val login: SharedFlow<LoginResultModel>
+        get() = _login
+
+    init {
+        viewModelScope.launchWithUiState {
+            user.collect {
+                _login.emit(LoginResultModel(it.id.isNotEmpty()))
+            }
+        }
+    }
+    fun kakaoLogin(loginKaKaoUseCase: LoginKaKaoUseCase) {
+        viewModelScope.launch {
+            loginKaKaoUseCase(Unit).collect {
+                it.onSuccess{
+                    _uiState.value = UiState.Success
+                }.onFailure {
+                    _uiState.value = UiState.Error
+                }
+            }
+        }
+    }
+
+    fun guestLogin(loginGuestUseCase: LoginGuestUseCase) {
+        viewModelScope.launch {
+            loginGuestUseCase(Unit).collect {
+                it.onComplete {
+                    _login.emit(LoginResultModel(true))
+                }
+            }
+        }
     }
 }
