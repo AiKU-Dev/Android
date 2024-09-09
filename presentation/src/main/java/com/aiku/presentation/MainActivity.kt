08212c2a92c7ea5dfd7e53ffde6baec9e1a3126a
scarlett0714 <tpttyakfn03@naver.com>
2024-09-10 02:13:50 +0900

    btmNav hotfix

--- presentation/src/main/java/com/aiku/presentation/MainActivity.kt
+++ presentation/src/main/java/com/aiku/presentation/MainActivity.kt
@@ -6,18 +6,25 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.aiku.domain.usecase.LoginUseCase
 import com.aiku.presentation.navigation.BtmNavGraph
+import com.aiku.presentation.navigation.SignUpNavGraph
 import com.aiku.presentation.theme.AiKUTheme
+import com.aiku.presentation.ui.screen.home.HomeScreen
+import com.aiku.presentation.ui.screen.login.composable.LoginScreen
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    @Inject
-    lateinit var loginUseCase: LoginUseCase
+    @Inject lateinit var loginUseCase: LoginUseCase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,10 +35,12 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    //SignUpNavGraph(navController = navController, loginUseCase)
-                    BtmNavGraph(navController)
-                }
+                            SignUpNavGraph(navController = navController, loginUseCase)
 
+                            BtmNavGraph(navController = navController)
+                        }
+
+                }
             }
         }
     }
