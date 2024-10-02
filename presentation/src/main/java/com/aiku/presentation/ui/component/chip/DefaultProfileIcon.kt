eb4abb6ceb3fb94c85b18f7d268e401a3d7ef7b1
Thirfir <sangbbang31@gmail.com>
2024-10-02 15:21:44 +0900

    기본 캐릭터 아이콘 패딩 조절

--- presentation/src/main/java/com/aiku/presentation/ui/component/chip/DefaultProfileIcon.kt
+++ presentation/src/main/java/com/aiku/presentation/ui/component/chip/DefaultProfileIcon.kt
@@ -9,6 +9,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.aiku.core.R
 import com.aiku.domain.model.group.type.ProfileBackground
@@ -32,10 +33,21 @@ fun DefaultProfileIcon(
         color = background.getColor(),
     ) {
         Image(
-            modifier = Modifier.fillMaxSize().padding(8.dp),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(32.dp),
             contentScale = ContentScale.Crop,
             painter = painterResource(id = character.getDrawableResId()),
             contentDescription = stringResource(id = R.string.profile_image_setup_content_description)
         )
     }
+}
+
+@Preview
+@Composable
+private fun DefaultProfileIconPreview() {
+    DefaultProfileIcon(
+        character = ProfileCharacter.C01,
+        background = ProfileBackground.BLUE
+    )
 }
