14769dc6a606c5b5b48b37426355fdde9842a1d3
Thirfir <sangbbang31@gmail.com>
2024-10-06 18:58:35 +0900

    DefaultAlertDialog 프리뷰

--- presentation/src/main/java/com/aiku/presentation/ui/component/dialog/DefaultAlertDialog.kt
+++ presentation/src/main/java/com/aiku/presentation/ui/component/dialog/DefaultAlertDialog.kt
@@ -3,10 +3,12 @@ package com.aiku.presentation.ui.component.dialog
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.AlertDialogDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.DialogProperties
@@ -43,5 +45,26 @@ fun DefaultAlertDialog(
         textContentColor = textContentColor,
         tonalElevation = tonalElevation,
         properties = properties
+    )
+}
+
+@Preview
+@Composable
+private fun DefaultAlertDialogPreview() {
+    DefaultAlertDialog(
+        onDismissRequest = {},
+        confirmButton = {
+            Text("확인")
+        },
+        dismissButton = {
+            Text("취소")
+        },
+        icon = {},
+        title = {
+            Text("다이얼로그 제목")
+        },
+        text = {
+            Text("다이얼로그 내용을 적어요 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.")
+        }
     )
 }
